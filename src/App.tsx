580d0937ad7a7b0d12613bed5a75697406e4b73d
AMINE SRIDI <sridiamine1@gmail.com>
2026-03-12 23:36:53 +0100

    DASHBOARD LIVE

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Terminal from './Terminal';
 import Dashboard from './Dashboard';
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
+        {/* La page d'accueil (Scanner) */}
         <Route path="/" element={<Terminal />} />
-        <Route path="/admin" element={<Dashboard />} />
+        
+        {/* LA PIÈCE MANQUANTE : Le Dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
