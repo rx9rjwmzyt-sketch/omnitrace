d9015cf6910b07b53f5a74bccbd9146dae7e90af
AMINE SRIDI <sridiamine1@gmail.com>
2026-03-14 01:13:32 +0100

    Hardcode des clés Supabase

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,8 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-// On récupère les clés du coffre-fort (Vite utilise import.meta.env)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// TEST NUCLÉAIRE : On colle directement les valeurs entre les guillemets simples (quotes)
+const supabaseUrl = 'https://wvcdrxnpztccpdcumgpy.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2Y2RyeG5wenRjY3BkY3VtZ3B5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzg0NTcsImV4cCI6MjA4ODgxNDQ1N30.FLvNdhfNByth7s18st4S0IutQZnmHb2HezI3llJCq_s';
 
-// On crée et on exporte le câble de connexion
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
